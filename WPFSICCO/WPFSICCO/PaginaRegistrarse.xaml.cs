bf98892b05b204b1f52cbe53c038e724c255e745
JorgeTorresSosa <44038945+JorgeTorresSosa@users.noreply.github.com>
2018-10-14 02:01:22 -0700

    Union con base de datos, combo box y fin de ingreso

--- WPFSICCO/WPFSICCO/PaginaRegistrarse.xaml.cs
+++ WPFSICCO/WPFSICCO/PaginaRegistrarse.xaml.cs
@@ -12,8 +12,13 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
-
+using System.Reflection;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using Email;
+using MySql.Data.MySqlClient;
+using System.Data.SqlClient;
+using System.Data;
 
 
 namespace WPFSICCO
@@ -37,6 +42,164 @@ namespace WPFSICCO
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+
+        private void CrearCuenta (object sender, RoutedEventArgs e)
+        {
+            if (CajaNombre.Text == "" || CajaApellidoPaterno.Text == "" || CajaApellidoMaterno.Text == "")
+            {
+                msgText.Text = "Apellidos o nombre en blanco";
+                Hecho.IsOpen = true;
+            }
+            else
+            {
+                if(CajaEdad.Text == "" )
+                {
+                    msgText.Text = "Edad no valida";
+                    Hecho.IsOpen = true;
+                }
+                else
+                {
+                    if(NoControl.Text == "")
+                    {
+                        msgText.Text = "Numero de control no valido";
+                        Hecho.IsOpen = true;
+                    }
+                    else
+                    {
+                        if(CajaEspecialidad.Text == "" || CajaSemestre.Text == "" || CajaEspecialidad.SelectedIndex == 0 || CajaSemestre.SelectedIndex == 0)
+                        {
+                            msgText.Text = "Especialidad o semestre no seleccionado";
+                            Hecho.IsOpen = true;
+                        }
+                        else
+                        {
+                            RegexUtilities utilities = new RegexUtilities();
+                            if (utilities.IsValidEmail(CorreoElc.Text) == false)
+                            {
+                                msgText.Text = "Correo electronico no valido";
+                                Hecho.IsOpen = true;
+                            }
+                            else
+                            {
+                                if(UsuarioRegistro.Text == "")
+                                {
+                                    msgText.Text = "Nombre de usuario no valido";
+                                    Hecho.IsOpen = true;
+                                }
+                                else
+                                {
+                                    if (Contra.Password == "" || Contra.Password != ConfContra.Password)
+                                    {
+                                        msgText.Text = "Las contraseñas no coinciden";
+                                        Hecho.IsOpen = true;
+                                    }
+                                    else
+                                    {
+                                        RegistrarenDB();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+           
+            
+
+        }
+
+        void RegistrarenDB()
+        {
+            MySqlConnection con = new MySqlConnection("server=localhost; user=root; database=base; SslMode=none");
+            
+            string Nombre, AP, AM, NUS, PSS, MAIL,C_PSS, N_CO_LENGHT;
+            int ESP=0, SEM, ED, NCO;
+                                        //Los comentarios significan el nombre de la variable en la DB
+            Nombre = CajaNombre.Text;   //Nombre
+            AP = CajaApellidoPaterno.Text;//Apellido_Paterno
+            AM = CajaApellidoMaterno.Text;//Apellido_Materno
+            NCO = Int32.Parse(NoControl.Text);//No_Control----Primary Key
+            NUS = UsuarioRegistro.Text;//Nombre_usuarios
+            PSS = Contra.Password;//Contraseña
+            MAIL = CorreoElc.Text;//Correo
+            C_PSS = ConfContra.Password;//
+            ED = Int32.Parse(CajaEdad.Text);//Edad
+            SEM = Int32.Parse(CajaSemestre.Text);//Semestre
+            N_CO_LENGHT = NoControl.Text;//Confirmacion de la contraseña
+            
+            
+                //Comparación de la especialidad---Buscar cómo cambiar la cajaespecialidad y semestre por una combobox o algo parecido        
+                switch(CajaEspecialidad.SelectedIndex)
+                {
+                    case 1:
+                        ESP = 1;
+                        break;
+                    case 2:
+                        ESP = 2;
+                        break;
+                    case 3:
+                        ESP = 3;
+                        break;
+                    case 4:
+                        ESP = 4;
+                        break;
+                    default:
+                        Error();
+                        break;
+                    
+                }
+                //Verificación de la longitud del numero de control para validacion de longitud en mysql
+                if(N_CO_LENGHT.Length>15)
+                {
+                    Error();
+                }
+                //error si la confirmacion de contraseña no coincide con la contraseña
+                if(PSS != C_PSS)
+                {
+                    Error();
+                }
+
+                if(SEM<=6 & SEM>=1)
+                {
+                    Error();
+                }
+
+            MySqlCommand comando = new MySqlCommand();
+            comando.CommandText = "Insert into usuario(Nombre, Apellido_Paterno, Apellido_Materno, Especialidad, Semestre, No_Control, Edad, Nombre_usuarios, Contraseña, Correo) values('" + Nombre + "', '" + AP + "', '" + AM + "',"+ESP+","+SEM+","+NCO+","+ED+",'" + NUS + "', '" + PSS + "', '" + MAIL + "')";
+            comando.Connection = con;
+            try
+            {
+                con.Open();
+                comando.ExecuteNonQuery();
+                MessageBox.Show("Usuario Registrado Correctamente");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error" + ex);
+
+            }
+
+        }
+        void Error()
+        {
+            MessageBox.Show("Error al introducir un dato");
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                MessageBox.Show("Abierto");
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+                MessageBox.Show("Error" + ex);
+            }
         }
     }
 }
